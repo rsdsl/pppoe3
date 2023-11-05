a6715cdc7b8a29c969f85ca50b15b0dec521dea6
HimbeerserverDE <himbeerserverde@gmail.com>
2023-11-05 14:52:50 +0100

    add mac address and magic number related error variants

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,11 @@ use thiserror::Error;
 /// An external error that prevents a supervisor from functioning.
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("no mac address on interface {0}")]
+    NoMacAddress(String),
+    #[error("no magic number negotiated locally")]
+    NoMagicNumber,
+
     #[error("io: {0}")]
     Io(#[from] io::Error),
     #[error("interface name contains nul byte: {0}")]
@@ -15,6 +20,8 @@ pub enum Error {
     #[error("error receiving from tokio watch channel: {0}")]
     WatchRecv(#[from] watch::error::RecvError),
 
+    #[error("error retrieving local mac address: {0}")]
+    MacAddress(#[from] mac_address::MacAddressError),
     #[error("ppproperly packet (de)serialization failed: {0}")]
     Ppproperly(#[from] ppproperly::Error),
 }
